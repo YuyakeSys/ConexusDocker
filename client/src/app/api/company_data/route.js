87629c1b9b9e4b997180b6666de85c321c90dbc4
CheserMeng <75210411+YuyakeSys@users.noreply.github.com>
2024-06-11 22:31:38 +0200

    Update route.js

--- client/src/app/api/company_data/route.js
+++ client/src/app/api/company_data/route.js
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 export async function GET(request) {
   const page = request.nextUrl.searchParams.get("page") || 1; // Get page from query params
   const response = await fetch(
-    `http://localhost:3000/users?page_type=companies&page=${page}`
+    `http://api:3000/users?page_type=companies&page=${page}`
   );
   const data = await response.json();
   return NextResponse.json(data);
