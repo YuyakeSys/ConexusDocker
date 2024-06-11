07c44af65d2924943580734221595edc6bf1469d
CheserMeng <75210411+YuyakeSys@users.noreply.github.com>
2024-06-11 21:03:26 +0200

    update local host

--- client/src/app/utils/constant.js
+++ client/src/app/utils/constant.js
@@ -6,8 +6,8 @@ export const USER_TYPES = {
 };
 
 export const API_URLS = {
-  BASIC_URL: "http://127.0.0.1:3000/api/v1/",
-  SERVER_URL: "http://127.0.0.1:3000",
+  BASIC_URL: "http://localhost:3000/api/v1/",
+  SERVER_URL: "http://localhost:3000",
 };
 
 export const INDUSTRY_AREAS = [
